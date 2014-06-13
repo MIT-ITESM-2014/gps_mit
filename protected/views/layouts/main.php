c4a790616010e46bec8b6b315f7cd1e8bb0e1aa2
arturo <arturo@arturo-Development.(none)>
2014-06-13 14:22:32 -0500

    Added views

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,8 +30,12 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Sample', 'url'=>array('/sample/index')),
+				array('label'=>'Identity', 'url'=>array('/identity/index')),
+				array('label'=>'Token', 'url'=>array('/token/index')),
+				array('label'=>'Truck', 'url'=>array('/truck/index')),
+				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
