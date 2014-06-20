9c57515ca3bafa59d743badc0329292e564cf9d0
Susie Cardenas <susie88@susie88-VirtualBox.(none)>
2014-06-20 11:36:04 -0400

    Added directions, corrected polylines and added traffic to google maps.

--- front/protected/views/identity/index.php
+++ front/protected/views/identity/index.php
@@ -2,6 +2,8 @@
 /* @var $this IdentityController */
 /* @var $dataProvider CActiveDataProvider */
 
+Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/maps/g_map_test.js');
+
 $this->breadcrumbs=array(
 	'Identities',
 );
@@ -18,3 +20,11 @@ $this->menu=array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
 )); ?>
+
+<h4> Map made with javascript and jquery</h4>
+
+<div id="map-canvas" style="height: 600px; width: 800px;">
+</div>
+
+<div id="map-legend" style=" background: white; padding: 10px; color: #EB3B45; border: 2px solid black; ">
+</div>
