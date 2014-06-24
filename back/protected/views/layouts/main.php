6ba6c05cc9cd3c60f9a5d6539ba695e62d2bc901
Susie Cardenas <susie88@susie88-VirtualBox.(none)>
2014-06-24 15:29:59 -0400

    Changed more of the backend layout

--- back/protected/views/layouts/main.php
+++ back/protected/views/layouts/main.php
@@ -26,26 +26,29 @@
 
 	<div id="header">
 		<div id="logo">
-			<div id="logo-image"> </div>
+			<a href="index.php" ><div id="logo-image"> </div></a>
 		</div>
 	</div><!-- header -->
 
-	<div id="mainmenu-container">
-		<div id="mainmenu">
-			<?php $this->widget('zii.widgets.CMenu',array(
-				'items'=>array(
-					array('label'=>'Home', 'url'=>array('/site/index')),
-					array('label'=>'Sample', 'url'=>array('/sample/index')),
-					array('label'=>'Identity', 'url'=>array('/identity/index')),
-					array('label'=>'Token', 'url'=>array('/token/index')),
-					array('label'=>'Truck', 'url'=>array('/truck/index')),
-					array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-					array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-				), 'id' => 'menu-elements' 
-			)); ?>
-		</div><!-- mainmenu -->
+	<div id="page-contents">
+		<div id="mainmenu-container">
+			<div id="mainmenu">
+				<?php $this->widget('zii.widgets.CMenu',array(
+					'items'=>array(
+						//array('label'=>'Home', 'url'=>array('/site/index')),
+						array('label'=>'Sample', 'url'=>array('/sample/index')),
+						array('label'=>'Identity', 'url'=>array('/identity/index')),
+						array('label'=>'Token', 'url'=>array('/token/index')),
+						array('label'=>'Truck', 'url'=>array('/truck/index')),
+						array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+						array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+					), 'id' => 'menu-elements' 
+				)); ?>
+			</div><!-- mainmenu -->
+		</div>
+		<?php echo $content; ?>
+		<div class="clear"> </div>
 	</div>
-	<?php echo $content; ?>
 
 	<div class="clear"></div>
 
--- back/protected/views/truck/_form.php
+++ back/protected/views/truck/_form.php
@@ -15,8 +15,6 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
@@ -38,8 +36,10 @@
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'CREATE' : 'Save', array('class'=>'button-style')); ?>
 	</div>
+
+	<p class="note"> <span class="required">* Required fields.</span></p>
 
 <?php $this->endWidget(); ?>
 
