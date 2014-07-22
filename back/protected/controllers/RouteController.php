36859232679afbbe00121522c8dfccb471c4742d
arturo <arturo@arturo-Development.(none)>
2014-07-22 14:26:23 -0500

    Added needed functions to Route Controller

--- back/protected/controllers/RouteController.php
+++ back/protected/controllers/RouteController.php
@@ -27,21 +27,21 @@ class RouteController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array(),
-				'users'=>array('*'),
-			),
+			//array('allow',  // allow all users to perform 'index' and 'view' actions
+			//	'actions'=>array(),
+			//	'users'=>array('*'),
+			//),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('index','view','admin'),
+				'actions'=>array('index','view'),
 				'users'=>array('@'),
 			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('delete','create','update',),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
+			//array('allow', // allow admin user to perform 'admin' and 'delete' actions
+			//	'actions'=>array('delete','create','update',),
+			//	'users'=>array('admin'),
+			//),
+			//array('deny',  // deny all users
+			//	'users'=>array('*'),
+			//),
 		);
 	}
 
