d4ed553616a2c65f9fc65c37d1febd7f1b4b536d
Susana Cardenas Molinar <scarmol@mit.edu>
2014-07-21 23:32:57 -0400

    Minor changes in front end change fleet view.

--- front/js/fleets/actions.js
+++ front/js/fleets/actions.js
@@ -1,1 +1,9 @@
 $('#icons-bar').hide();
+
+
+$('<td id="prueba"></td>').prependTo('table > thead > tr.filters');
+//$('.filters').append('<td id="prueba"></td>');
+
+$('#prueba').addClass('hola');
+
+$('.filters td:nth-child(2)').append('<input id="fleet-search-form">');
--- front/protected/views/company/change.php
+++ front/protected/views/company/change.php
@@ -4,31 +4,35 @@
 	<div id="select-fleet-text"> Select a fleet to continue. </div>
 </div>	
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'identity-company-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'summaryText'=>' ',
-	'htmlOptions' => array('class' => 'gridStyle'),
-	'pager' => array('cssFile' => Yii::app()->baseUrl . '/css/changeCompany.css', 'header' => ' '),	
-	'cssFile' => Yii::app()->baseUrl . '/css/changeCompany.css',
-	'columns'=>array(
-	  array(
-	    'header'=>'Fleet Name',
-	    'value'=>'$data->company->name',
-	  ),
-		array(
-			'class'=>'CButtonColumn',
-			'template'=>'{select}',
-			'buttons'=>array(
-			  'select' => array(
-			    'label'=>'Select',
-			    'url'=>'Yii::app()->createUrl("company/change", array("company"=>$data->company_id))',
-			  ),
+<div class="choose-fleet-list-container">
+	<div class="choose-fleet-filter-background"></div>
+	<?php $this->widget('zii.widgets.grid.CGridView', array(
+		'id'=>'identity-company-grid',
+		'dataProvider'=>$model->search(),
+		'filter'=>$model,
+		'summaryText'=>' ',
+		'htmlOptions' => array('class' => 'gridStyle'),
+		'pager' => array('cssFile' => Yii::app()->baseUrl . '/css/changeCompany.css', 'header' => ' '),	
+		'cssFile' => Yii::app()->baseUrl . '/css/changeCompany.css',
+		'columns'=>array(
+		  array(
+		    'header'=>'Fleet Name',
+		    'value'=>'$data->company->name',
+		  ),
+			array(
+				'class'=>'CButtonColumn',
+				'template'=>'{select}',
+				'buttons'=>array(
+				  'select' => array(
+				    'label'=>' ',
+				    'options' => array('class'=> 'go-to-map-button'),
+				    'url'=>'Yii::app()->createUrl("company/change", array("company"=>$data->company_id))',
+				  ),
+				),
 			),
 		),
-	),
-)); ?>
+	)); ?>
+</div>
 
 <?php
   Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/fleets/actions.js',CClientScript::POS_END);
