44d41191db0daa3ffce2f4448d4af95eec75ec61
Susana Cardenas Molinar <scarmol@mit.edu>
2014-07-23 19:16:36 -0400

    Minor changes to views.

--- back/protected/views/company/successful_reset.php
+++ back/protected/views/company/successful_reset.php
@@ -1,3 +1,8 @@
+<div class="headers">
+	<h1>Reset Successful </h1>
+</div>	
+
+<div class="clear"></div>
 
 <div class="reset-form-container">
 	All the information of the fleet <?php echo Yii::app()->user->getState('current_company_name'); ?> has been deleted.
