93eb4771082f5e2d788521bf2f4003d0b12a8cc6
arturo <arturo@arturo-Development.(none)>
2014-07-24 11:07:37 -0500

    seeds

---
+++ back/db/seeds.sql
@@ -0,0 +1,2 @@
+insert into identity (name, last_name, username, password, created_at, updated_at, is_admin) values ('Edgar', 'Blanco', 'edgar', '123456', current_timestamp, current_timestamp, 1);
+
