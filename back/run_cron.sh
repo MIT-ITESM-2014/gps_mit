ba4da57eb8a66f2f0abc08bf897c8d14b9df32b6
arturo <arturo@arturo-Development.(none)>
2014-07-31 18:03:04 -0500

    Create sh for cron

---
+++ back/run_cron.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if ! [ -f /home/itesm-student/projects/compass/back/mutex/cron_mutex.file ]
+then
+  touch /home/itesm-student/projects/compass/back/mutex/cron_mutex.file
+  /usr/local/php/bin/php /home/itesm-student/projects/compass/back/cron.php processing
+  rm /home/itesm-student/projects/compass/back/mutex/cron_mutex.file
+else
+fi
