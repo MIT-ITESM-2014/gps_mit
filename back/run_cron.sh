d17106b0d10368561a2cd9f89b42cd13beb96252
arturo <arturo@arturo-Development.(none)>
2014-07-31 23:58:36 -0500

    Fixing cron shell

--- back/run_cron.sh
+++ back/run_cron.sh
@@ -5,5 +5,4 @@ then
   touch /home/itesm-student/projects/compass/back/mutex/cron_mutex.file
   /usr/local/php/bin/php /home/itesm-student/projects/compass/back/cron.php processing
   rm /home/itesm-student/projects/compass/back/mutex/cron_mutex.file
-else
 fi
